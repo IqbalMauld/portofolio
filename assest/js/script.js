faea93cb0c7eb74f2eb501168277c7377a50aa37
IqbalMauld <140826458+IqbalMauld@users.noreply.github.com>
2024-09-07 21:41:15 +0700

    Update script.js

--- assest/js/script.js
+++ assest/js/script.js
@@ -3,4 +3,12 @@ const menu = document.getElementById('menu');
 
 menu.addEventListener('click', () =>{
     nav.classList.toggle('active');
+
 });
+
+document.addEventListener('click', function(e) {
+    if (!menu.contains(e.target) && !nav.contains(e.target)){
+        nav.classList.remove('active');
+    }
+});
+
